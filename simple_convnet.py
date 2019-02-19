7e5f7a2c3321fa3bee9d561635437dd5c2123ab3
Kohei Moroi <koheimoroi@Koheis-MacBook-Air.local>
2019-02-20 00:40:22 +0900

    removed unnecessary comments

--- simple_convnet.py
+++ simple_convnet.py
@@ -9,21 +9,20 @@ from common.gradient import numerical_gradient
 
 
 class SimpleConvNet:
-    """単純なConvNet
-
+    """
     conv - relu - pool - affine - relu - affine - softmax
-    
+
     Parameters
     ----------
-    input_size : 入力サイズ（MNISTの場合は784）
-    hidden_size_list : 隠れ層のニューロンの数のリスト（e.g. [100, 100, 100]）
-    output_size : 出力サイズ（MNISTの場合は10）
+    input_size : MNIST=784
+    hidden_size_list : （e.g. [100, 100, 100]）
+    output_size : MNIST=10
     activation : 'relu' or 'sigmoid'
-    weight_init_std : 重みの標準偏差を指定（e.g. 0.01）
-        'relu'または'he'を指定した場合は「Heの初期値」を設定
-        'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
+    weight_init_std : （e.g. 0.01）
+    'relu' or 'he'
+    'sigmoid' or 'xavier'
     """
-    def __init__(self, input_dim=(1, 28, 28), 
+    def __init__(self, input_dim=(1, 28, 28),
                  conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
                  hidden_size=100, output_size=10, weight_init_std=0.01):
         filter_num = conv_param['filter_num']
@@ -34,7 +33,6 @@ class SimpleConvNet:
         conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
         pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
 
-        # 重みの初期化
         self.params = {}
         self.params['W1'] = weight_init_std * \
                             np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
@@ -46,7 +44,6 @@ class SimpleConvNet:
                             np.random.randn(hidden_size, output_size)
         self.params['b3'] = np.zeros(output_size)
 
-        # レイヤの生成
         self.layers = OrderedDict()
         self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
                                            conv_param['stride'], conv_param['pad'])
@@ -64,40 +61,38 @@ class SimpleConvNet:
 
         return x
 
+    """
+    x: input
+    t: teaching
+    """
     def loss(self, x, t):
-        """損失関数を求める
-        引数のxは入力データ、tは教師ラベル
-        """
         y = self.predict(x)
         return self.last_layer.forward(y, t)
 
     def accuracy(self, x, t, batch_size=100):
         if t.ndim != 1 : t = np.argmax(t, axis=1)
-        
+
         acc = 0.0
-        
+
         for i in range(int(x.shape[0] / batch_size)):
             tx = x[i*batch_size:(i+1)*batch_size]
             tt = t[i*batch_size:(i+1)*batch_size]
             y = self.predict(tx)
             y = np.argmax(y, axis=1)
-            acc += np.sum(y == tt) 
-        
+            acc += np.sum(y == tt)
+
         return acc / x.shape[0]
 
     def numerical_gradient(self, x, t):
-        """勾配を求める（数値微分）
-
+        """
         Parameters
         ----------
-        x : 入力データ
-        t : 教師ラベル
+        x : input
+        t : teaching
 
         Returns
-        -------
-        各層の勾配を持ったディクショナリ変数
-            grads['W1']、grads['W2']、...は各層の重み
-            grads['b1']、grads['b2']、...は各層のバイアス
+            grads['W1']、grads['W2']
+            grads['b1']、grads['b2']
         """
         loss_w = lambda w: self.loss(x, t)
 
@@ -109,19 +104,17 @@ class SimpleConvNet:
         return grads
 
     def gradient(self, x, t):
-        """勾配を求める（誤差逆伝搬法）
-
+        """
         Parameters
         ----------
-        x : 入力データ
-        t : 教師ラベル
+        x : input
+        t : teaching
 
         Returns
-        -------
-        各層の勾配を持ったディクショナリ変数
-            grads['W1']、grads['W2']、...は各層の重み
-            grads['b1']、grads['b2']、...は各層のバイアス
+            grads['W1']、grads['W2']
+            grads['b1']、grads['b2']
         """
+
         # forward
         self.loss(x, t)
 
@@ -134,14 +127,13 @@ class SimpleConvNet:
         for layer in layers:
             dout = layer.backward(dout)
 
-        # 設定
         grads = {}
         grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
         grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
         grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
 
         return grads
-        
+
     def save_params(self, file_name="params.pkl"):
         params = {}
         for key, val in self.params.items():
